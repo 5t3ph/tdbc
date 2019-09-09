04dc702e2d43a62a734c703d6bcaed317df0680d
5t3ph <thinkdobecreate@gmail.com>
2019-09-08 21:07:45 -0500

    Resolve form issues

--- src/js/form.js
+++ src/js/form.js
@@ -2,31 +2,31 @@ const form = document.querySelector("form");
 const formOpenTrigger = document.getElementById("open-form");
 const formCloseTrigger = document.getElementById("close-form");
 
+function closeForm() {
+  form.classList.remove("visible");
+  formOpenTrigger.focus();
+}
+
 formOpenTrigger.addEventListener("click", function(e) {
   form.classList.add("visible");
   document.getElementById("name").focus();
 });
 
 formCloseTrigger.addEventListener("click", function(e) {
-  form.classList.remove("visible");
-  formOpenTrigger.focus();
+  closeForm();
 });
 
 function handleSubmit(e) {
-  const values = {
-    "form-name": "contact",
-    name: document.getElementById("name").value,
-    email: document.getElementById("email").value,
-    message: document.getElementById("message").value
-  };
-
   return fetch("/", {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: encode(values)
+    body: serialize(form)
   })
-    .then(() => alert("Success!"))
-    .catch(error => alert(error));
+    .then(function() {
+      form.innerHTML =
+        '<p class="form-notice">Thanks! I\'ll respond as soon as I am able. Feel free to reach out on <a href="https://twitter.com/5t3ph">Twitter</a>.</p><button id="close-form" class="button button--cancel" type="button">Close Form</button >';
+    })
+    .catch(error => alert("Oops! Can you please try again?"));
 }
 
 form.addEventListener("submit", function(e) {
