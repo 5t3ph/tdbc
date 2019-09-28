fbc71efc1f68ab32b8d4d1b04aa0f24ca06bfbc1
5t3ph <thinkdobecreate@gmail.com>
2019-09-28 17:37:08 -0500

    Update classes in form script

--- src/js/form.js
+++ src/js/form.js
@@ -3,32 +3,32 @@ const formOpenTrigger = document.getElementById("open-form");
 const formCloseTrigger = document.getElementById("close-form");
 
 function closeForm() {
-  form.classList.remove("visible");
+  form.classList.remove("tdbc-visible");
   formOpenTrigger.focus();
 }
 
 formOpenTrigger.addEventListener("click", function(e) {
-  form.classList.add("visible");
+  form.classList.add("tdbc-visible");
   document.getElementById("name").focus();
 });
 
 document.addEventListener("click", function(e) {
-  if (e.target && e.target.id == "close-form") {
+  if (e.target && e.target.id == "tdbc-close-form") {
     closeForm();
   }
-}); 
+});
 
 function handleSubmit(e) {
   return fetch("/", {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: serialize(form)
+    body: serialize(form),
   })
     .then(function() {
       form.innerHTML =
-        '<p class="form-notice">Thanks! I\'ll respond as soon as I am able. Feel free to reach out on <a href="https://twitter.com/5t3ph">Twitter</a>.</p><button id="close-form" class="button button--cancel" type="button">Close Panel</button >';
+        '<p class="tdbc-form-notice">Thanks! I\'ll respond as soon as I am able. Feel free to reach out on <a href="https://twitter.com/5t3ph">Twitter</a>.</p><button id="close-form" class="tdbc-button tdbc-button--cancel" type="button">Close Panel</button >';
     })
-    .catch(error => alert("Oops! Can you please try again?"));
+    .catch((error) => alert("Oops! Can you please try again?"));
 }
 
 form.addEventListener("submit", function(e) {
