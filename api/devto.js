d14616fc75007de41e09057384e8fbe03108f452
5t3ph <thinkdobecreate@gmail.com>
2020-04-25 10:29:41 -0500

    try 6

--- api/devto.js
+++ api/devto.js
@@ -6,27 +6,22 @@ exports.handler = async (event, context, callback) => {
   try {
     const items = await axios.get(apiRoot, { headers: { "api-key": process.env.DEVTO } });
 
+    let response = [];
+
+    // Grab the items and smoosh them into something the front-end will like
+    if (items.length) {
+      response = items.map((item) => ({
+        title: item.title,
+        url: item.url,
+        description: item.description,
+        tags: item.tag_list.join(", "),
+      }));
+      //.reverse();
+    }
     callback(null, {
       statusCode: 200,
-      body: JSON.stringify(items),
+      body: JSON.stringify(response),
     });
-
-    // let response = [];
-
-    // // Grab the items and smoosh them into something the front-end will like
-    // if (items.length) {
-    //   response = items.map((item) => ({
-    //     title: item.title,
-    //     url: item.url,
-    //     description: item.description,
-    //     tags: item.tag_list.join(", "),
-    //   }));
-    //   //.reverse();
-    // }
-    // callback(null, {
-    //   statusCode: 200,
-    //   body: JSON.stringify(response),
-    // });
   } catch (err) {
     callback(err);
   }
