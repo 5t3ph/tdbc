9ca976635c1435e6ba490755a75b6e41210ad23c
5t3ph <thinkdobecreate@gmail.com>
2020-08-12 14:47:10 -0500

    Fix CSS build step

--- .eleventy.js
+++ .eleventy.js
@@ -10,7 +10,7 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addWatchTarget("./src/sass/");
 
-  eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addPassthroughCopy("./src/css/style*");
   eleventyConfig.addPassthroughCopy("./src/fonts");
   eleventyConfig.addPassthroughCopy("./src/img");
   eleventyConfig.addPassthroughCopy("./src/robots.txt");
